4b6bc8b0db51269364d82d9dc8002654c2785bfc
RehnQ <51484513+RehnQ@users.noreply.github.com>
2020-02-09 11:59:57 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -47,20 +47,18 @@ function isAllTrue(array, fn) {
    isSomeTrue([1, 2, 30, 4, 5], n => n > 20) // вернет true
    isSomeTrue([1, 2, 3, 4, 5], n => n > 20) // вернет false
  */
+
 function isSomeTrue(array, fn) {
-    if (!Array.isArray(array) || array.length === 0) {
-        throw Error('empty array');
-    }
-    if (typeof(fn) != 'function') {
-        throw Error('fn is not a function');
-    }
-    for (var i = 0; i < array.length; i++) {
-        if (!fn(array[i])) {
-            return true;
-        }
+    if (!Array.isArray(array) || array.length === 0)
+        throw new Error('empty array');
+    if (typeof fn != 'function')
+        throw new Error('fn is not a function');
+    for (let i = 0; i < array.length; i++) {
+        if (fn(array[i])) return true;
     }
     return false;
 }
+
 
 /*
  Задание 3:
